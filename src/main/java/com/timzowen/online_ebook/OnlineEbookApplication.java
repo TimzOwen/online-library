d9b3f5060de9b60ba235b8e613adc36bd0e64b7e
Timz Owen <timzowen@Timzs-MacBook-Pro.local>
2025-05-17 20:55:57 +0300

    Ignoring HELP.md, mvnw, and mvnw.cmd

---
+++ src/main/java/com/timzowen/online_ebook/OnlineEbookApplication.java
@@ -0,0 +1,13 @@
+package com.timzowen.online_ebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineEbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineEbookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/timzowen/online_ebook/OnlineEbookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.timzowen.online_ebook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineEbookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
